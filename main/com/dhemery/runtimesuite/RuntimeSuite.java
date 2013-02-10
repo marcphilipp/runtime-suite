02149e36f94b710b777ba0bfc7fb8f0413ed1be7
Marc Philipp <mail@marcphilipp.de>
2013-02-11 00:26:09 +0100

    Remove unused import

--- main/com/dhemery/runtimesuite/RuntimeSuite.java
+++ main/com/dhemery/runtimesuite/RuntimeSuite.java
@@ -2,7 +2,6 @@ package com.dhemery.runtimesuite;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
